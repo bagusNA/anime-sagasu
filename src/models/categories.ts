7d2671e4ac68184f0ef756f1bef2d1cca6b44b68
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2022-06-30 16:01:13 +0800

    Add search method for every categories

--- src/models/categories.ts
+++ src/models/categories.ts
@@ -1,20 +1,34 @@
 import type { Category } from "./types/Category";
+import Anime from "./Anime";
+import Manga from "./Manga";
 
 export const categories: Category[] = [
   {
     id: 0,
     name: 'All',
+    search: async (query: string) => {
+      return await (await Anime.getSearch(query)).data;
+    },
   },
   {
     id: 1,
     name: 'Anime',
+    search: async (query: string) => {
+      return await (await Anime.getSearch(query)).data;
+    },
   },
   {
     id: 2,
     name: 'Manga',
+    search: async (query: string) => {
+      return await (await Manga.getSearch(query)).data;
+    },
   },
   {
     id: 3,
     name: 'Light Novel',
+    search: async (query: string) => {
+      return await (await Manga.getNovelSearch(query)).data;
+    },
   }
 ];
--- src/models/types/Category.ts
+++ src/models/types/Category.ts
@@ -1,4 +1,5 @@
 export interface Category {
   id: Number,
-  name: string,
+  name: String,
+  search: Function
 }
