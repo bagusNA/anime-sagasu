77f43abbee90f00fae17744942c5a7498a34e5de
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2022-07-03 15:00:21 +0800

    uh

--- src/views/AnimeDetailsView.vue
+++ src/views/AnimeDetailsView.vue
@@ -1,19 +1,35 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useRoute } from 'vue-router';
-// import Anime from "@/models/Anime";
+import Anime from '@/models/Anime';
+import type { Anime as IAnime} from '@/models/types/Anime';
 
 const route = useRoute();
 const id = parseInt(route.params.id as string);
-console.log(id)
-// const data = ref(await Anime.getAnimeFullById(id));
+const data = ref<IAnime>(await (await Anime.getAnimeFullById(id)).data);
+
 </script>
 
 <template>
-  <div>
-    {{ id }}
+ <div class="anime-container">
+    <div class="header-wrap">
+      <div class="banner">
+        
+      </div>
+      <div class="header">
+
+      </div>
+    </div>
+    <div class="content">
+      <aside class="content__aside">
+
+      </aside>
+      <div class="content__main">
+
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-
 </style>
